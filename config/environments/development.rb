4f51195e7851428c25d0ae007ac67cfc76ac78f4
Yunan <yunanelfath@ymail.com>
2016-08-13 17:13:10 +0700

    add dinamis asset host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV['ASSETS_HOST'] ||= 'http://localhost:3000'
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
